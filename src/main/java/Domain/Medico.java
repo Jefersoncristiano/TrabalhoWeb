e78e6e06b13eb61d6d32055fa9ed007185731388
Jeferson Cristiano <jefersoncristianosantos@gmail.com>
2025-04-02 20:31:39 -0300

    Primeiro commit

---
+++ src/main/java/Domain/Medico.java
@@ -0,0 +1,62 @@
+package Domain;
+
+public class Medico {
+
+private Integer id;
+private String nome;
+private String telefone;
+private String email;
+private Integer CRM;
+private String especialidade;
+
+public Medico() {}
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+        System.out.printf("");
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getCRM() {
+        return CRM;
+    }
+
+    public void setCRM(Integer CRM) {
+        this.CRM = CRM;
+    }
+
+    public String getEspecialidade() {
+        return especialidade;
+    }
+
+    public void setEspecialidade(String especialidade) {
+        this.especialidade = especialidade;
+    }
+}
